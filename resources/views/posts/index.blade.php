2700c62b40c4768679337f6f8b99e9cf095bfcac
arinkoariari <yamaguchi.kohei.47s@st.kyoto-u.ac.jp>
2022-12-02 13:29:37 +0000

    12/2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,34 @@
+ <body>
+        <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                    <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                    </form>
+                </div>
+                <a href="">{{ $post->category->name }}</a>
+            @endforeach
+        </div>
+         <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+    
+    <script>
+        function deletePost(id) {
+                'use strict'
+        
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+    </script>
+</html>
